77badeaddce91c1580770740af59c729731fe4c1
unlocker <blueraptor1001@gmail.com>
2021-05-27 01:58:06 -0700

    Update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,14 @@
-async function main() {}
+async function main() {
+  const VaultFactory = await ethers.getContractFactory("Vault");
+  const Vault = await VaultFactory.deploy();
+  console.log("Vault deployed at ", Vault.address);
+
+  const ZapperFactory = await ethers.getContractFactory("Zapper");
+  const Zapper = await ZapperFactory.deploy(Vault.address);
+  console.log("Zapper deployed at ", Zapper.address);
+
+  await Vault.setZapper(Zapper.address);
+}
 
 main()
   .then(() => process.exit(0))
