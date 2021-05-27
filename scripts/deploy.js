f76c567d766144c8d411f7f75be530fe8f9e5051
unlocker <blueraptor1001@gmail.com>
2021-05-27 01:27:21 -0700

    Update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,8 @@
-async function main() {
-  const SaffronERC20Staking = await ethers.getContractFactory(
-    "SaffronERC20Staking"
-  );
-  const saffronStaking = await SaffronERC20Staking.deploy();
-  console.log("SaffronERC20Staking deployed to:", saffronStaking.address);
-}
+async function main() {}
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
